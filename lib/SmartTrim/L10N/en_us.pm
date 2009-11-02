43d6aa99875494f14eac1fb13ab39ca1b5e78e8c
padawan <info@padawan.info>
2009-11-02 15:10:39 +1100

    Added L10N files and French localization.

---
+++ lib/SmartTrim/L10N/en_us.pm
@@ -0,0 +1,15 @@
+# Movable Type (r) (C) 2005-2009 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id: en_us.pm 3455 2009-02-23 02:29:31Z auno $
+
+package SmartTrim::L10N::en_us;
+
+use strict;
+
+use base 'SmartTrim::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
---
+++ lib/SmartTrim/L10N/fr.pm
@@ -0,0 +1,20 @@
+# Movable Type (r) (C) 2005-2009 Six Apart, Ltd. All Rights Reserved.
+# This code cannot be redistributed without permission from www.sixapart.com.
+# For more information, consult your Movable Type license.
+#
+# $Id$
+
+package SmartTrim::L10N::fr;
+
+use strict;
+use base 'SmartTrim::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+	'A global modifier to trim strings by length without cutting words.' => 'Un filtre global permettant de couper une chaîne à une longueur donnée sans couper au milieu d\'un mot'.',
+	);
+
+1;
+
