aaf1e77615215d1f6bbe3885f3b81e11118341ab
padawan <info@padawan.info>
2010-11-09 17:11:03 +1100

    New version based on HTML::Tidy with better HTML handling.

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
-**SmartTrim** is a plugin for Movable Type 4 that aims to provide a smarter trimming global modifier than _words_ or *trim_to*.
+**SmartTrim** is a plugin for Movable Type that aims to provide a smarter trimming global modifier than `words` or `trim_to`.
 
-The stock MT global modifier _words_ cuts at a certain number of words without any control on the resulting length, it will also remove all HTML tags. *trim_to* cuts to a specific length but can cut a string in the middle of a word and can also output invalid HTML in the form of unclosed tags.
+The stock MT global modifier `words` cuts at a certain number of words without any control on the resulting length, it will also remove all HTML tags. `trim_to` cuts to a specific length but can cut a string in the middle of a word and can also output invalid HTML in the form of unclosed tags.
 
 SmartTrim:
 
 * trims a string to a target character length without cutting words
-* will close any unclosed tags if the source contains HTML
+* will close any unclosed tags if the source contains HTML _and_ HTML::Tidy is present (otherwise any HTML will be removed before trimming occurs)
 * can append an optional ending string like an ellipsis to the result
 
 # Syntax
 
-Once SmartTrim is installed in the MT plugins directory, the global modifier `smarttrim` can be used with two parameters: a _length_ value (required) and an optional _suffix_ as an ending string. E.g.:
+Once SmartTrim is installed in the MT plugins directory, the global modifier `smarttrim` can be used with four parameters: a _length_ value (required), a _suffix_ as an ending string, a _type_ and a list of _options_ (those last three arguments are optional). E.g.:
 
-	<$mt:EntryBody smarttrim="[-|+]length"[,"suffix"]$>
+	<$mt:EntryBody smarttrim="[-|+]length"[,"suffix"[,"type"[,"options"]]]$>
 
 _length_ is an integer defining the target length. _length_ can be prefixed with a + or - sign to influence the behavior of SmartTrim as follows:
-
 
 * "-_length_" will trim the string at the word ending immediately before or at the specified length, ensuring that the result has a length that is inferior or equal to length.
 * "+_length_" will include the word that would otherwise be cut at the specified length, if any.
@@ -26,49 +25,55 @@ If a _suffix_ string is provided _and_ trimming occurs then it is appended to th
 
 You can use a single character such as the ellipsis (…) or a string such as " (…)" (note the leading space), provided it does not start with a digit. Note that the ending is appended as is, so start with a space if you want a space between the resulting string and the ending. Also note that the ending length is _not_ factored in the target length calculations.
 
+
+_type_ defines the type of output and can take two values: "html" or "xml" (lowercase). If _type_ is omitted or different from those values, then it defaults to "xhtml".
+
+
+_options_ is a list of any number of HTML::Tidy arguments strung together as `key:value` pairs and separated by semicolons. Run `tidyp -help-config` for the complete list.
+
 ## Examples
 
 * `<$mt:EntryBody smarttrim="40"$>` will result in a string that has a length closest to 40 characters
 * `<$mt:EntryBody smarttrim="-40"$>` will result in a string that has a maximum length of 40 characters
 * `<$mt:EntryBody smarttrim="+40"$>` will result in a string that has a length of more or less 40 characters whether a word may be cut at 40, in which case it is included in the result
 * `<$mt:EntryBody smarttrim="-40","…"$>` will trim the source at a maximum length of 40 characters, and add an ellipsis if a cut is performed
+* `<$mt:EntryBody smarttrim="40","…", "html", "numeric_entities:1;drop-empty-paras:1"$>` will result in a string that has a length closest to 40 characters, any HTML will be tidied up with special chars replaced with numeric entities and empty paragraphs dropped
 
 # Caveats
 
-If _SmartTrim_ closes an unclosed HTML tag, the closing tag will be prefixed with the following comment: `<!-- close mismatch -->` (this is a feature of HTML::Defang).  
-HTML tags will be closed only if they belong to this hard-coded list: `a img table tbody thead tr td th font div span pre center p em strong i b q cite blockquote dl dd ul ol li h1 h2 h3 h4 h5 h6 fieldset tt`.
+If HTML::Tidy is not installed, any HTML in the source string will be removed _before_ trimming occurs (this is a safeguard to prevent the output of malformed HTML). In effect, this is equivalent to `<$mt:EntryBody remove_html="1" smarttrim="…"$>`, which you can use to force removal of HTML.
 
-If the source contains HTML, the length of the HTML code counts against the target length. This means that the _"visible"_ result may appear shorter than the target length. However, the actual length of the resulting string may be bigger than the expect length if unclosed tags are closed.
+If HTML::Tidy is present and the source contains HTML, the HTML code counts against the target length. This means that the _rendered_ result may appear shorter than the target length. However, the actual length of the resulting string may be bigger than the expected length if unclosed tags are closed.
 
-If the source string contains HTML and you need to get rid of it, use the `remove_html` attribute _before_ `smarttrim`, like so:
-
-	<$mt:EntryBody remove_html="1" smarttrim="40"$>
-
-If you place the `remove_html` modifier after `smarttrim`, any HTML code in the source string will count against the length calculations.
+If _SmartTrim_ cuts within the start of an HTML block before its enclosing content, it may output an empty block, such as `<a href="…"></a>`.
 
 # Installation
 
+* Install <a href="https://github.com/petdance/tidyp">tidyp</a>
+* Install <a href="https://github.com/petdance/html-tidy">html-tidy</a> (or <a href="http://search.cpan.org/~petdance/HTML-Tidy/">HTML::Tidy</a> from CPAN)
 * Download <a href="http://github.com/padawan/smarttrim" onClick="javascript: pageTracker._trackPageview('/software/smarttrim.github');">SmartTrim</a>
 * Uncompress and copy the SmartTrim folder into your MT plugins directory.
 
 # Release history
 
+* Version 2.0 - 2010/11/09 - Switch to HTML::Tidy in replacement of HTML::Defang.
 * Version 1.1.2 - 2010/01/05 - Relaxing the sanitization done by HTML::Defang.
 * Version 1.1.1 - 2009/11/02 - Added L10N files and French translation.
 * Version 1.1 - 2009/10/22 - Modified the arguments syntax. Added HTML::Defang to close HTML tags. Added POD.
 * Version 1.0 - 2009/10/19 - First public version.
 
-# TODO
+# NOTE
 
-Make the tag closing list a modifiable preference of the plugin.
+Once you have installed HTML::Tidy, you might also be interested by the <a href="https://github.com/movabletype/mt-plugin-tidings/">Tidings plugin</a> for MT.
 
 # Tribute
 
 This plugin has been inspired by the <a href="http://plugins.movabletype.org/trimwordsbylen/">TrimWordsbyLen</a> plugin from Crys Clouse.  
 Thanks to Su, Brice, Steeve, Michael and Byrne for the feedback and suggested improvements.
+Thanks to Andy Lester for HTML::Tidy and tidyp.
 
 # Copyright & License
 
-Copyright (C) 2009 François Nonnenmacher, Ubiquitic.
+Copyright (C) 2009-2010 François Nonnenmacher, Ubiquitic.
 
 This free software is provided as-is WITHOUT ANY KIND OF GUARANTEE; you can redistribute it and/or modify it under the same terms as Perl itself.
--- lib/SmartTrim/Plugin.pm
+++ lib/SmartTrim/Plugin.pm
@@ -3,63 +3,101 @@
 # A Movable Type text filter to trim strings by length without cutting words.
 # http://ubiquitic.com/software/smarttrim-movable-type-plugin.html
 #
-# Release 1.1.2 — 2010/01/05 - Relax Defang behavior
-# ----------------------------------------------------------------------------
-# This free software is provided as-is WITHOUT ANY KIND OF GUARANTEE.
-# You may use it for commercial or personal use.
-# If you distribute it, please keep this notice intact.
+# Release 2.0 — 2010/11/09 - Switch to HTML::Tidy + mt-tidings
 #
 # Copyright (C) François Nonnenmacher, Ubiquitic. http://ubiquitic.com/
+# ----------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation, or (at your option) any later version.  
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+# version 2 for more details.  You should have received a copy of the GNU 
+# General Public License version 2 along with this program. If not, see 
+# <http://www.gnu.org/licenses/>.
+# If you distribute it, please keep this notice intact.
 # ----------------------------------------------------------------------------
 
 package SmartTrim::Plugin;
 
-#use strict;
+use strict;
 use MT 4.0;
-use HTML::Defang;
 use MT::Util qw( remove_html );
 
 sub smarttrim {
-	my ($s, $attr)  = @_;
-	my ($offset, $len, $end, $result) = '';
+    my ($s, $attr)  = @_;
+    my ($offset, $len, $end, $result, $type, $tidyp_options) = '';
 
-	if (ref($attr) eq 'ARRAY') {
-		($offset, $len) = $attr->[0] =~ m/^(\D?)(\d+)/;
-		$end = $attr->[1];
-	} else { # Supporting the old 1.0 format
-		($offset, $len, $end) = $attr =~ m/^(\D?)(\d+)(.*?)$/;
-	}
-	return $s unless ($len > 0);
+    if (ref($attr) eq 'ARRAY') {
+        ($offset, $len) = $attr->[0] =~ m/^(\D?)(\d+)/;
+        $end = $attr->[1]; # String to append, if trimming occurs
+        $type = $attr->[2]; # Type of HTML, if needs handling
+        $tidyp_options = $attr->[3]; # Options for tidying the HTML.
+    } else { # Supporting the old 1.0 format
+        ($offset, $len, $end) = $attr =~ m/^(\D?)(\d+)(.*?)$/;
+    }
+    return $s unless ($len > 0);
 
-	if ($offset eq '+') { $result = cut_after($len, $s); }
-	elsif ($offset eq '-') { $result = cut_before($len, $s); }
-	else {
-		my $before = cut_before($len,$s);
-		my $after = cut_after($len,$s);
-		my $afterdelta = length($after) - $len;
-		my $beforedelta = $len - length($before);	
-		$result = ($afterdelta < $beforedelta) ? $after : $before ;
-	}
+    my $htmlsource = ($result ne remove_html($result)) ? 1:0; # Source contains HTML
+    my $tidy = eval { require HTML::Tidy; HTML::Tidy->new or die; }; # I can handle HTML
 
-	if (($end ne '') && ($result ne $s)) { $result .= $end; }
+    if ($htmlsource && !$tidy) { # Lets get rid of the HTML since I cannot handle unclosed tags
+        $s = remove_html($s);
+    }
 
-	if ($result ne remove_html($result)) { # source contains HTML, let's make sure tags are closed
-		my @mismatched_tags = qw(a img table tbody thead tr td th font div span pre center p em strong i b q cite blockquote dl dd ul ol li h1 h2 h3 h4 h5 h6 fieldset tt iframe);
-		my $Defang = HTML::Defang->new(context => $Self, fix_mismatched_tags => 1, mismatched_tags_to_fix => \@mismatched_tags, url_callback => \&DefangUrlCallback, attribs_callback => \&DefangAttribsCallback );
-		$result = $Defang->defang($result);
-	}
+    if ($offset eq '+') { $result = cut_after($len, $s); }
+    elsif ($offset eq '-') { $result = cut_before($len, $s); }
+    else {
+        my $before = cut_before($len,$s);
+        my $after = cut_after($len,$s);
+        my $afterdelta = length($after) - $len;
+        my $beforedelta = $len - length($before);   
+        $result = ($afterdelta < $beforedelta) ? $after : $before ;
+    }
 
-	return $result;
-  }
+    if (($end ne '') && ($result ne $s)) { $result .= $end; }
+
+    if ($htmlsource && $tidy) { # Source contains HTML, let's make sure tags are closed
+        require HTML::Tidy;
+        my $params;
+        $params->{show_warnings} = 0;
+        $params->{show_body_only} = 1;
+        if ( $tidyp_options ) {
+            my @options = split(/;/, $tidyp_options);
+            foreach my $option ( @options ) {
+                my @results = split(/:/, $option);
+                next unless ( scalar @results == 2 );
+                my $key = $results[0];
+                my $val = $results[1];
+                $key =~ s|\s*(.*)\s*|$1|;
+                $val =~ s|\s*(.*)\s*|$1|;
+                $params->{$key} = $val;
+            }
+        }
+        if ( $type eq 'html' ) {
+            $params->{output_html} = 1;
+        }
+        elsif ( $type eq 'xml' ) {
+            $params->{output_xml} = 1;
+        }
+        else {
+            $params->{output_xhtml} = 1;
+        }
+        my $tidy = HTML::Tidy->new($params);
+        $result = $tidy->clean($result);
+    }
+
+    return $result;
+}
 
 sub cut_before {
     my $len = shift;
     my $s = shift;
-    if (length($s) > $len)
-      {
+    if (length($s) > $len) {
         $s = substr($s, 0, $len+1);
         $s =~ s/(.*)\s.*/$1/; 
-      }
+    }
     return $s;
 }
 
@@ -70,21 +108,9 @@ sub cut_after {
     return $s;
 }
 
-# Callback for custom handling URLs in HTML attributes as well as style tag/attribute declarations
-sub DefangUrlCallback {
-    my ($Self, $Defang, $lcTag, $lcAttrKey, $AttrValR, $AttributeHash, $HtmlR) = @_;
-    return 0; # Explicitly allow all URLs in tag attributes
-}
-
-# Callback for custom handling HTML tag attributes
-sub DefangAttribsCallback {
-    my ($Self, $Defang, $lcTag, $lcAttrKey, $AttrValR, $HtmlR) = @_;
-    return 0; # Don't Defang attributes
-  }
-
 =head1 NAME
 
-SmartTrim::Plugin - Movable Type global modifier to trim a string to a target length without cutting in middle of words.
+SmartTrim::Plugin - A Movable Type global modifier to trim a string to a target length without cutting in the middle of words.
 
 Plugin page: L<http://ubiquitic.com/software/smarttrim-movable-type-plugin.html>
 
@@ -94,6 +120,8 @@ Plugin page: L<http://ubiquitic.com/software/smarttrim-movable-type-plugin.html>
 
     <$mt:EntryBody remove_html="1" smarttrim="-40"$>
 
+    <$mt:EntryBody smarttrim="40","…", "html", "numeric_entities:1;drop-empty-paras:1"$>
+
 =head1 DESCRIPTION
 
 I<SmartTrim> trims a string to a target character length without cutting words, and can append an optional ending string to the result.
@@ -101,9 +129,9 @@ If the source string contains HTML, I<SmartTrim> will close any unclosed tag.
 
 =head1 PARAMETERS
 
-I<SmartTrim> accepts two parameters: a length value (required) and an optional suffix as an ending string.
+I<SmartTrim> accepts four parameters: a length value (required), an optional suffix as an ending string, an optional type and an optional list of options.
 
-    smarttrim="[-|+]I<length>"[,"I<suffix>"]
+    smarttrim="[-|+]I<length>"[,"I<suffix>"][,"I<type>"][,"I<options>"]
 
 =head2 I<length>
 
@@ -129,16 +157,23 @@ If I<length> is not prefixed by either - or + then the string is trimmed as clos
 
 If a I<suffix> string is provided and trimming occurs then it is appended to the result.
 
+=head2 I<type>
+
+I<type> defines the type of output and can take two values: I<html> or I<xml> (lowercase). If I<type> is omitted or different from those values, then it defaults to XHTML.
+
+=head2 I<options>
+
+I<options> is a list of options passed to tidyp, in the form "key: value; key: value". See C<tidyp -help-config> for the complete list.
+
 =head1 CAVEATS
 
-If I<SmartTrim> closes an unclosed HTML tag, the closing tag will be prefixed with the following comment: <!-- close mismatch --> (this is a feature of Defang).
-HTML tags will be closed only if they belong to this hard-coded list: table tbody thead tr td th font div span pre center p em strong i b q cite blockquote dl dd ul ol li h1 h2 h3 h4 h5 h6 fieldset tt
+If the source contains HTML, the length of the HTML code counts against the target length. This means that the I<visible> result may appear shorter than the target length. However, the actual length of the resulting string may be bigger than the expected length if unclosed tags are closed.
 
-If the source contains HTML, the length of the HTML code counts against the target length. This means that the I<visible> result may appear shorter than the target length. However, the actual length of the resulting string may be bigger than the expect length if unclosed tags are closed.
+If I<SmartTrim> cuts within the start of an HTML block before its enclosing content, it may output an empty block, such as <a href="…"></a>.
 
-=head1 DEPENDANCIES
+=head1 DEPENDENCIES
 
-I<SmartTrim> depends on I<HTML::Defang> (included).
+I<SmartTrim> requires Tidyp and I<HTML::Tidy> by Andy Lester.
 
 =head1 TRIBUTE
 
